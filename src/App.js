5eca37c05a6088af7b35cd88a7d761cf5ec8aca2
Laura Saltaren <lau.saltaren96@gmail.com>
2020-12-29 19:53:40 +0100

    add links and RandomColorSquare

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Counter from './Counter/Counter';
+import Menu from './Menu/Menu';
+import MenuItem from './Menu/MenuItem/MenuItem';
+import RandomColorSquare from './RandomColorSqueare/RandomColorSquare';
+import SpecialButton from './SpecialButton/SpecialButton';
+import Toggle from './Toggle/Toggle';
 
-function App() {
-  return (
+class App extends Component{
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <Menu>
+           <MenuItem route="/">Home</MenuItem>
+           <MenuItem route="/">About</MenuItem>
+           <MenuItem route="/">Services</MenuItem>
+           <MenuItem route="/">Contact us</MenuItem>
+           <hr/>
+           <SpecialButton/>
+           <hr/>
+           <Counter/>
+           <Toggle/>
+           <RandomColorSquare/>
+         </Menu>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
